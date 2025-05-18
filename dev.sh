2f6e2c66c5470b0bb1eb9c062616249634e57151
Mark Eschbach <meschbach@gmail.com>
2025-05-18 09:33:14 -0700

    cd/pr: more feedback when building programs

--- dev.sh
+++ dev.sh
@@ -5,9 +5,12 @@ function compile() {
   entry=$1; shift
   output=$1; shift
 
+  echo "Compiling $output"
   go build -o $output ./$entry
 }
 
 mkdir -p bin
 compile cmd/competition bin/competition
 compile cmd/simple bin/simple
+
+echo "All programs compiled."
