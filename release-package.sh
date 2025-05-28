19717df838349a72501805fdddf1a06966890cf5
Mark Eschbach <meschbach@gmail.com>
2025-05-28 16:16:01 -0700

    delivery/release: adds versioning and prefixes to the generated artifacts

--- release-package.sh
+++ release-package.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+ver=${1:-dev}
+
 set -e
 outputs="darwin-arm64 linux-arm64 linux-amd64"
 for output in ${outputs}; do
-  tar zcvf $output.tgz -C "bin/${output}" .
+  target_file="npcs-${ver}-$output.tgz"
+  echo $target_file
+  tar zcf $target_file -C "bin/${output}" .
 done
